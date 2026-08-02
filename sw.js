dd029268fc8284d62cb7972a24856b746a62b461
cayman4i-lang <cayman4i@gmail.com>
2026-08-02 13:37:11 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='loose-point-app-v31-github-pages';
+const SHELL=['/','/index.html','/styles.css','/game.js','/manifest.webmanifest','/icon.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    if(new URL(event.request.url).origin===self.location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
