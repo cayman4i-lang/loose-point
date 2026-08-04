ee0434e1417088d34cfb612a9d3e551a00b140bc
cayman4i-lang <cayman4i@gmail.com>
2026-08-04 12:49:33 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='loose-point-app-v33-ability-road-portal-moon';
+const CACHE='loose-point-app-v40-shield-collision';
 const SHELL=['/','/index.html','/styles.css','/game.js','/manifest.webmanifest','/icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
