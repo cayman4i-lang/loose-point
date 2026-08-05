5d55efaeae2d8de9e20d8dd69c36b854c347a518
cayman4i-lang <cayman4i@gmail.com>
2026-08-05 11:30:12 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='loose-point-app-v60-direct-input-focus';
+const CACHE='loose-point-app-v61-standalone-inputs';
 const SHELL=['/','/index.html','/styles.css','/game.js','/manifest.webmanifest','/icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
