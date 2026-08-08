638b02a71d8ef815d35dfa7788f1e633f6aaf99c
cayman4i-lang <cayman4i@gmail.com>
2026-08-08 13:32:02 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='loose-point-app-v71-independent-arm-slots';
+const CACHE='loose-point-app-v72-independent-arms-barrier';
 const SHELL=['/','/index.html','/styles.css','/game.js','/manifest.webmanifest','/icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
